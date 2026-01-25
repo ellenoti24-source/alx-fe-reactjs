7e4730790d47b9f16cfd16f1c9d3c9c066e2c12e
Ellen Abena Korang Oti <ellenoti24@gmail.com>
2026-01-25 23:06:20 +0000

    i

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,10 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AddRecipeForm from './components/AddRecipeForm';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
-import SearchBar from './components/SearchBar';
-import FavoritesList from './components/FavoritesList';
-import RecommendationsList from './components/RecommendationsList';
 
 const App = () => {
   return (
@@ -12,11 +9,7 @@ const App = () => {
       <div>
         <h1>Recipe Sharing App</h1>
 
-        <SearchBar />
         <AddRecipeForm />
-
-        <FavoritesList />
-        <RecommendationsList />
 
         <Routes>
           <Route path="/" element={<RecipeList />} />
