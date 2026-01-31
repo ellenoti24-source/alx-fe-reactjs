ab43e673f12a9257d6e83ef3596ef045407baab3
Ellen Abena Korang Oti <ellenoti24@gmail.com>
2026-01-31 11:11:08 +0000

    ee

---
+++ github-user-search/github-user-search/src/components/Search.jsx
@@ -0,0 +1,10 @@
+const Search = () => {
+  return (
+    <div>
+      <h2>GitHub User Search</h2>
+      <input type="text" placeholder="Search GitHub username..." />
+    </div>
+  );
+};
+
+export default Search;
