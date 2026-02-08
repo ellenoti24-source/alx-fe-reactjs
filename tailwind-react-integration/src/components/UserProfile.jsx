2237a62dcac7b76e2aaa421b3928c404ff9b0ed3
Ellen Abena Korang Oti <ellenoti24@gmail.com>
2026-02-08 14:27:03 +0000

    wee

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ function UserProfile() {
   return (
     <div className="
       bg-gray-100
-      p-4 md:p-8
+     sm:p-4 md:p-8
       max-w-xs md:max-w-sm
       mx-auto my-20
       rounded-lg
@@ -14,7 +14,7 @@ function UserProfile() {
         alt="User"
         className="
           rounded-full
-          w-24 h-24
+         sm:w-24 sm:h-24
           md:w-36 md:h-36
           mx-auto
         "
