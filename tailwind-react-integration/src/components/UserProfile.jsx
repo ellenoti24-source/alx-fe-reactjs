3561d3ba53d348ee040afa9d181b897014e93bd8
Ellen Abena Korang Oti <ellenoti24@gmail.com>
2026-02-08 14:30:17 +0000

    wyy

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,11 +2,14 @@ function UserProfile() {
   return (
     <div className="
       bg-gray-100
-     sm:p-4 md:p-8
+      sm:p-4 md:p-8
       max-w-xs md:max-w-sm
       mx-auto my-20
       rounded-lg
       shadow-lg
+      hover:shadow-xl
+      transition-shadow
+      duration-300
     ">
 
       <img
@@ -14,24 +17,31 @@ function UserProfile() {
         alt="User"
         className="
           rounded-full
-         sm:w-24 sm:h-24
+          sm:w-24 sm:h-24
           md:w-36 md:h-36
           mx-auto
+          hover:scale-110
+          transition-transform
+          duration-300
+          ease-in-out
         "
       />
 
       <h1 className="
-        text-lg md:text-xl
+        sm:text-lg md:text-xl
         text-blue-800
         my-4
         text-center
+        hover:text-blue-500
+        transition-colors
+        duration-300
       ">
         John Doe
       </h1>
 
       <p className="
         text-gray-600
-        text-sm md:text-base
+        sm:text-sm md:text-base
         text-center
       ">
         Developer at Example Co. Loves to write code and explore new technologies.
