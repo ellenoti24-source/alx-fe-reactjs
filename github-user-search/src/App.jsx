be5ce4a04f1bb40626e4f609d02e76c75e19cd09
Ellen Abena Korang Oti <ellenoti24@gmail.com>
2026-02-08 14:46:43 +0000

    weeee

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>GitHub User Search App</h1>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
---
+++ github-user-search/src/components/Search.jsx
@@ -0,0 +1,10 @@
+const Search = () => {
+  return (
+    <div className="search-container">
+      <h2>GitHub User Search</h2>
+      <input type="text" placeholder="Search GitHub users..." />
+    </div>
+  );
+};
+
+export default Search;
