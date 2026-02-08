d5e2e419fd014c65efcadd3ac0d800361b6f3092
Ellen Abena Korang Oti <ellenoti24@gmail.com>
2026-02-08 15:04:52 +0000

    qeee

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="app-container">
       <h1>GitHub User Search App</h1>
+      <h1 className="text-3xl font-bold text-center text-blue-600">
+  Tailwind is working!
+</h1>
       <Search />
     </div>
   );
