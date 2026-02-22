a55bb1d81842542d52af894decacd3c14463660d
Ellen Abena Korang Oti <ellenoti24@gmail.com>
2026-02-22 12:27:41 +0000

    week w

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,20 +1,27 @@
+// QueryClient QueryClientProvider queryClient client={queryClient} PostsComponent
+
+import { QueryClient, QueryClientProvider } from "react-query";
 import { useState } from "react";
 import PostsComponent from "./components/PostsComponent";
 
 function App() {
+  const queryClient = new QueryClient();
+
   const [showPosts, setShowPosts] = useState(true);
 
   return (
-    <div>
-      <h1>React Query Demo</h1>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <h1>React Query Demo</h1>
 
-      <button onClick={() => setShowPosts(!showPosts)}>
-        {showPosts ? "Hide Posts" : "Show Posts"}
-      </button>
+        <button onClick={() => setShowPosts(!showPosts)}>
+          {showPosts ? "Hide Posts" : "Show Posts"}
+        </button>
 
-      {showPosts && <PostsComponent />}
-    </div>
+        {showPosts && <PostsComponent />}
+      </div>
+    </QueryClientProvider>
   );
 }
 
-export default App;
+export default App;+
