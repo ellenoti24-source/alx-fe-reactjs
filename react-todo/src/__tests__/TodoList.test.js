bb8ce5b16fe32bd1baa2ac427a5011ffeb149fd6
Ellen Abena Korang Oti <ellenoti24@gmail.com>
2026-02-22 13:32:12 +0000

    week

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,34 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import TodoList from "../components/TodoList";
+
+describe("TodoList Component", () => {
+  test("renders initial todos", () => {
+    render(<TodoList />);
+    expect(screen.getByText("Learn React")).toBeInTheDocument();
+    expect(screen.getByText("Write Tests")).toBeInTheDocument();
+  });
+
+  test("adds a new todo", () => {
+    render(<TodoList />);
+    fireEvent.change(screen.getByPlaceholderText(/add todo/i), {
+      target: { value: "New Todo" },
+    });
+    fireEvent.click(screen.getByText("Add"));
+    expect(screen.getByText("New Todo")).toBeInTheDocument();
+  });
+
+  test("toggles a todo", () => {
+    render(<TodoList />);
+    const todo = screen.getByText("Learn React");
+    fireEvent.click(todo);
+    expect(todo).toHaveStyle("text-decoration: line-through");
+  });
+
+  test("deletes a todo", () => {
+    render(<TodoList />);
+    const todo = screen.getByText("Learn React");
+    const deleteButton = todo.nextSibling;
+    fireEvent.click(deleteButton);
+    expect(todo).not.toBeInTheDocument();
+  });
+});
